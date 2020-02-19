c5a8b7b4e0ac657011fc7a6a7a4bc400af1bcad6
Danny Cairns <dpcairns@gmail.com>
2020-02-19 10:58:13 -0800

    add weather endpoint

--- app.js
+++ app.js
@@ -1,8 +1,44 @@
 const express = require('express');
 const data = require('./geo.js');
+const weather = require('./darksky.js');
 const app = express();
-const request = require('superagent');
+// const request = require('superagent');
 
+app.get('/', (request, respond) => respond.send('Jello World!'));
+
+app.get('/location', (request, respond) => {
+    const cityData = data.results[0];
+
+    respond.json({
+        formatted_query: cityData.formatted_address,
+        latitude: cityData.geometry.location.lat,
+        longitude: cityData.geometry.location.lng, 
+    });
+});
+
+const getWeatherData = (lat, lng) => {
+    return weather.daily.data.map(forecast => {
+        return {
+            forecast: forecast.summary,
+            time: new Date(forecast.time),
+        };
+    })
+} ;
+app.get('/weather', (req, res) => {
+    // use the lat and lng from earlier to get weather data for the selected area
+    const portlandWeather = getWeatherData(/*lat*lng*/);
+    
+    // res.json that weather data in the appropriate form
+    res.json(portlandWeather);
+});
+
+app.get('*', (req, res) => res.send('404!!!!!!'))
+
+module.exports = {
+    app: app,
+};
+
+/*
 console.log(data)
 // do not modify this URL!
 const URL = 'https://rickandmortyapi.com/api/episode';
@@ -17,24 +53,11 @@ const getEpisodesNamesWithSomeChar = async (someId) => {
     .filter(episode => episode.characters.includes(`https://rickandmortyapi.com/api/character/${someId}`))
     .map(filteredEpisode => filteredEpisode.name)
 }
-
-app.get('/', (request, respond) => respond.send('Jello World!'));
-
 app.get('/about', (request, respond) => respond.json({
     name: 'About me!!!!',
     time: Date.now(),
     number: Math.random()
 }));
-
-app.get('/location', (request, respond) => {
-    const cityData = data.results[0];
-
-    respond.json({
-        formatted_query: cityData.formatted_address,
-        latitude: cityData.geometry.location.lat,
-        longitude: cityData.geometry.location.lng, 
-    });
-});
 
 app.get('/get-episodes/:latitude/:longitude/', async (request, respond) => {
     const episodes = await getEpisodesNamesWithSomeChar(Number(request.params.characterId));
@@ -47,9 +70,4 @@ app.get('/get-episodes/:latitude/:longitude/', async (request, respond) => {
         episodes: episodes,
     });
 });
-
-app.get('*', (req, res) => res.send('404!!!!!!'))
-
-module.exports = {
-    app: app,
-};
+*/
---
+++ step.md
@@ -0,0 +1,5 @@
+1. install express and create app
+1. create location route that reads from query params, gets some dummy data, and returns json
+1. deploy to heroku using heroku cli
+1. make a `/weather` endpoint that uses the fetched lat/lng (we will NOT use query params) to get weather data from a dummy json file. 
+1. return 8 days of dummy weather data from the `/weather` endpoint.
